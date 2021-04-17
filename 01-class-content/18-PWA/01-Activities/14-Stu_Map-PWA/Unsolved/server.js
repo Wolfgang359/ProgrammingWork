613b8016ca15d3b628132aad6070366d990d6e3a
Michael A Llanes <wolfgangllanes@gmail.com>
2021-04-17 11:50:58 -0500

    Testing shft+alt+f command

--- 01-class-content/18-PWA/01-Activities/14-Stu_Map-PWA/Unsolved/server.js
+++ 01-class-content/18-PWA/01-Activities/14-Stu_Map-PWA/Unsolved/server.js
@@ -5,7 +5,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 require("./routes/apiRoutes")(app);
